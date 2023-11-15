6a5bb83ac061eb7016098e0a151521def27f1856
Bolormaa <135795277+bolourima@users.noreply.github.com>
2023-11-15 19:10:04 +0800

    add element

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("addBtn");
+let i = 0;
+function addBox() {
+  i++;
+  const box = document.createElement("div");
+  box.classList.add("box");
+  box.innerText = i;
+  document.body.appendChild(box);
+}
+btn.addEventListener("click", addBox);
